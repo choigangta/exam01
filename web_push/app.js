5f393dccdd4247ae2ff59a5ce39e6b474c6fe361
Gang Ta <gangta@gmail.com>
2019-03-29 17:30:53 +0900

    test

---
+++ web_push/app.js
@@ -0,0 +1,30 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBb4i0z7aZG21fgG0ScDi5OCmE5TOz4jBI",
+    authDomain: "pricetrackingbycream.firebaseapp.com",
+    databaseURL: "https://pricetrackingbycream.firebaseio.com",
+    projectId: "pricetrackingbycream",
+    storageBucket: "pricetrackingbycream.appspot.com",
+    messagingSenderId: "844789212498"
+};
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+    .then(function () {
+        console.log('Have permission');
+        return messaging.getToken();
+    })
+    .then(function () {
+        console.log("token: ", token);
+    })
+    .catch(function (err) {
+        console.log('Error Occured.');
+
+    });
+
+messaging.onMessage(function (playload) {
+    console.log('onMessage: ', playload)
+});
