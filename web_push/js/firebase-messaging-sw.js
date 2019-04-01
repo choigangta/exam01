98c562a5694aa6c8d745e366f9b5d8c706802227
Gang Ta <gangta@gmail.com>
2019-04-01 11:30:55 +0900

    TEST

--- web_push/js/firebase-messaging-sw.js
+++ web_push/js/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/5.9.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/5.9.1/firebase-messaging.js')
+// importScripts('https://www.gstatic.com/firebasejs/5.9.1/firebase-messaging.js')
+importScripts('./firebase-messaging.js')
 
 // Initialize Firebase
 var config = {
